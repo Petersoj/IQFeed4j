b7ca0c66cb25af27a7306ba45215b0224557b650
Jacob Peterson <Petersoj@users.noreply.github.com>
2021-07-16 21:30:27 -0600

    Modify editorconfig to not use wildcard so much

--- src/main/java/net/jacobpeterson/iqfeed4j/feed/streaming/derivative/DerivativeFeed.java
+++ src/main/java/net/jacobpeterson/iqfeed4j/feed/streaming/derivative/DerivativeFeed.java
@@ -22,7 +22,14 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.checkArgument;
