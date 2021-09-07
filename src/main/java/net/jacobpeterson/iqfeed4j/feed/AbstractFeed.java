a332a7edfe42cd77f18ce54fd7833a880b84ed87
Jacob Peterson <Petersoj@users.noreply.github.com>
2021-09-06 20:33:24 -0600

    Update to 6.2-1.5.1

--- src/main/java/net/jacobpeterson/iqfeed4j/feed/AbstractFeed.java
+++ src/main/java/net/jacobpeterson/iqfeed4j/feed/AbstractFeed.java
@@ -208,7 +208,7 @@ public abstract class AbstractFeed implements Runnable {
                     cleanupState();
                     return;
                 } else {
-                    // Removed in production for version 6.2-1.5
+                    // Removed in production for version 6.2-1.5.1
                     LOGGER.trace("Received message line: {}", line);
 
                     String[] csv = csvSplitter.splitToList(line).toArray(new String[0]);
